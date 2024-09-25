7413d51bb7a8b1102999eb9dd32e089a2598af66
Alianur Rahman <71885945+AlianurRahman@users.noreply.github.com>
2024-09-25 11:30:32 +0700

    Complete deploy.py

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,15 +1,20 @@
 # -*- coding: utf-8 -*-
 
 """
-This script is used to do prediction based on trained model
+This script is used to do deploy based on latest trained model
 
 Usage:
-    python3 ./scripts/predict.py
+    python3 ./scripts/deploy.py
 
 """
 from huggingface_hub import notebook_login
 
+from train import train
+
+def deploy():
+    notebook_login()
+    model = train()
+    model.push_to_hub()
 
 if __name__ == '__main__':
-    notebook_login()
-    trainer.push_to_hub()
+    deploy()
